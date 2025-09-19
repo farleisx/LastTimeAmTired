10a2348ddd610d3d979ebb11eebe6d3388b2c29f
farleisx <laroxmafox@gmail.com>
2025-09-19 17:18:47 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import LoginSignup from "./components/LoginSignup";
+import Dashboard from "./components/Dashboard";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
